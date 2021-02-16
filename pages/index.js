294a29c9a29bc0ff0c96ccaf913fcc11f39292aa
Daniel Vogel <danielcvt@gmail.com>
2021-02-16 17:46:09 -0300

    lista-de-req

--- pages/index.js
+++ pages/index.js
@@ -4,43 +4,43 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>VogelCodes.com - Iniciando na programação aos 30</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Bem Vindo{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+        <h1 className="text-2xl font-bold">
+          Como construí{' '}
+          <a className="text-blue-500" href="https://vogelcodes.com">
+          meu site
           </a>
+          {' '}em 15 minutos.
+          
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+          Usando apenas: GitHub, NextJS, Vercel.
         </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <div
+            className="p-6 mt-6 text-left border w-96 rounded-xl"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">GitHub &rarr;</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+              Criar conta uma conta gratuita em{' '}<a             href="https://github.com"
+>https://github.com</a>, para salvar o código da nossa página em um local seguro independente de hospedagem.
+              
             </p>
-          </a>
+          </div>
 
           <a
             href="https://nextjs.org/learn"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">NextJS &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Framework para criação de sites modernos usando NodeJS. A maneira mais <strong>simples</strong> de colocar sua página no ar, tendo controle total sobre o código.
             </p>
           </a>
 
@@ -48,9 +48,9 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <h3 className="text-2xl font-bold">Vercel &rarr;</h3>
             <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+              Serviço de hospedagem gratuito para projetos web. Associando a conta GitHub pode fazer o carregamento - <strong className="bg-gray-100">Deploy</strong> - de qualquer repositório na plataforma em minutos.
             </p>
           </a>
 
