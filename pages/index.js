7a44485b2516e1b6eb115f019e29684c985aae40
Daniel Vogel <danielcvt@gmail.com>
2021-02-16 16:46:18 -0300

    traducao-titulo

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Bem Vindo{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
